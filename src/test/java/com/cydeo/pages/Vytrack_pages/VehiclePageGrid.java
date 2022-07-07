67497ce39cf53e337de64ded67ea65e9cd67bd89
zivadinjo <zivadinjo1985@gmail.com>
2022-07-07 00:01:52 -0500

    start

---
+++ src/test/java/com/cydeo/pages/Vytrack_pages/VehiclePageGrid.java
@@ -0,0 +1,38 @@
+package com.cydeo.pages.Vytrack_pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class VehiclePageGrid {
+
+    public VehiclePageGrid(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(partialLinkText = "Fleet")
+    public WebElement feetMenu;
+
+    @FindBy(partialLinkText = "Vehicles")
+    public WebElement vehicles;
+
+    @FindBy(partialLinkText = "Reset")
+    public WebElement gridResetBtn;
+
+    @FindBy(xpath = "//i[@class='fa-cog hide-text']")
+    public WebElement gridSettingsBtn;
+
+
+    public boolean isButtonOnRight(String buttonRight,String buttonLeft){
+
+        boolean check = false;
+        String xPath;
+
+
+
+        return check;
+
+    }
+
+}
---
+++ src/test/java/com/cydeo/step_definitions/GridResetStepDef.java
@@ -0,0 +1,36 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GridResetStepDef {
+
+
+    @Given("the Sales Manager is on Vytrack homepage")
+    public void the_sales_manager_is_on_vytrack_homepage() {
+
+    }
+    @When("the Sales Manager clicks on Fleet dropdown")
+    public void the_sales_manager_clicks_on_fleet_dropdown() {
+
+    }
+    @Then("the Sales Manager clicks on Vehicles under fleet dropdown")
+    public void the_sales_manager_clicks_on_vehicles_under_fleet_dropdown() {
+
+    }
+
+    @Given("the Sales Manager is on Vehicle page")
+    public void the_sales_manager_is_on_vehicle_page() {
+
+    }
+    @When("the Sales Manager sees Grid Settings is on the right side of the Reset button and on the right side of the page")
+    public void the_sales_manager_sees_grid_settings_is_on_the_right_side_of_the_reset_button_and_on_the_right_side_of_the_page() {
+
+    }
+    @Then("the Sales Manager should be able to click Grid Settings button")
+    public void the_sales_manager_should_be_able_to_click_grid_settings_button() {
+
+    }
+
+}
