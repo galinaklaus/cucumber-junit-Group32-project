c6001c3b180b71f0c45066dbcdb25b53af8226ad
galinaklaus <98919194+galinaklaus@users.noreply.github.com>
2022-07-14 12:05:02 -0400

    Update CukesRunner.java

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin ={"html:target/cucumber-report.html",
-        "json:target/cucumber-reports/cucumber.json"},
+        "json:target/cucumber.json"},
         features = "src/test/resources/features" ,
         glue = "com/cydeo/step_definitions" ,
         dryRun = false,
